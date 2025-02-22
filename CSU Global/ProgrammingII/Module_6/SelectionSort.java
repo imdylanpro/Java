08ede9a0c977195ad0d9f0653d89b76e8bfe61ce
Dylan Nelson <159092190+imdylanpro@users.noreply.github.com>
2025-02-22 10:44:45 -0800

    Update SelectionSort.java
    Added small structure improvements.

--- CSU Global/ProgrammingII/Module_6/SelectionSort.java
+++ CSU Global/ProgrammingII/Module_6/SelectionSort.java
@@ -2,6 +2,11 @@
 // February 22, 2025
 // SelectionSort.java
 
-public static class SelectionSort {
-    
+import java.util.Comparator;
+
+public static class SelectionSort implements Comparator {
+
+    public int compare(Object o1, Object o2) {
+        
+    }
 }
